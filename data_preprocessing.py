d82b0f4402b6c5b08cdf337f81db04171b5f825a
andyBarbaro <barbaro.3@osu.edu>
2022-04-20 02:26:37 -0400

    add package; update types of data returned

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,6 +7,7 @@ import re
 
 import librosa
 import torch
+import numpy as np
 
 SEC_PER_MIN = 60
 
@@ -14,13 +15,13 @@ def generate_lyrics_label():
     for filename in glob.glob(os.path.join(os.getcwd() + '\Model_Data\lyrics', '*.txt')):
         with open(filename, 'r') as r:
             lines = r.readlines()
-            label_list = [0] * 2 * SEC_PER_MIN 
+            label_list = [0] * 2 * SEC_PER_MIN
             for line in lines:
                 timestamp = re.split(r'\[|\]', line)[1]
                 time = re.split(r'[.:]', timestamp)
                 # time format: [min, sec, minisec]
                 if int(time[0]) < 2:
-                    sec = int(time[1]) + int(time[0]) * SEC_PER_MIN 
+                    sec = int(time[1]) + int(time[0]) * SEC_PER_MIN
                     label_list[sec] = 1
             with open(filename.split('.')[0] + ".label", 'w') as w:
                 w.write(' '.join(str(s) for s in label_list))
@@ -37,15 +38,15 @@ def zip_label_wav():
     label_list = []
     for filename in os.scandir(reg_dir):
         print("Loading file:", filename.name)
-        reg_wav, reg_sr = librosa.load(filename.path, duration=120)
-        iso_wav, iso_sr = librosa.load(iso_dir + filename.name, duration=120)
+        reg_wav, _ = librosa.load(filename.path, sr=22016, duration=120)
+        iso_wav, _ = librosa.load(iso_dir + filename.name, sr=22016, duration=120)
         with open(label_dir + filename.name.split('.')[0] + '.label', 'r') as r:
             line = r.readline()
             label_list.append(str(line).split(' '))
             r.close()
-        reg_wav_list.append((reg_wav, reg_sr))
-        iso_wav_list.append((iso_wav, iso_sr))
-    label_torch = torch.from_numpy(np.array(label_list)).float()
+        reg_wav_list.append(reg_wav)
+        iso_wav_list.append(iso_wav)
+    label_torch = torch.from_numpy(np.float_(label_list)).float()
     return label_torch, reg_wav_list, iso_wav_list
 
 if __name__ == '__main__':
@@ -53,4 +54,3 @@ if __name__ == '__main__':
     generate_lyrics_label()
     # Pack the label and .wav file
     iso_labels, iso_wavs, reg_wavs = zip_label_wav()
-        
