a481be01cf832b4222b7a50bc410463fc12d29d0
andyBarbaro <barbaro.3@osu.edu>
2022-04-20 12:04:16 -0400

    model runs but needs to be fine tuned. loss function is still having issues

--- pause_model.py
+++ pause_model.py
@@ -12,6 +12,8 @@ from data_preprocessing import *
 class PauseNet1(nn.Module):
   def __init__(self, num_mfccs, hid1, hid2, out):
     super().__init__()
+    self.sigmoid = nn.Sigmoid()
+    self.relu = nn.ReLU()
     self.fc1 = nn.Linear(num_mfccs, hid1)
     torch.nn.init.xavier_uniform_(self.fc1.weight)
     self.fc2 = nn.Linear(hid1, hid2)
@@ -22,22 +24,20 @@ class PauseNet1(nn.Module):
 
   # pass forward for nn
   def forward(self, x):
-    x=self.fc1(x)
-    x=F.sigmoid(x)
-    x=self.fc2(x)
-    x=F.sigmoid(x)
-    x=self.fc3(x)
+    x=self.relu(self.fc1(x))
+    x=self.relu(self.fc2(x))
+    x=self.sigmoid(self.fc3(x))
 
     return x
 
 
 if __name__ == '__main__':
     #set parameters
-    num_epochs = 3
-    num_mfccs = 13
+    num_epochs = 30
+    num_mfccs = 20
     hidden1_size = 100
     hidden2_size = 25
-    out_size = 2
+    out_size = 1
 
     # set other constants
     sr = 22050
@@ -46,7 +46,7 @@ if __name__ == '__main__':
 
     # create neural network
     pause_net = PauseNet1(num_mfccs, hidden1_size, hidden2_size, out_size)
-    optimizer = optim.Adam(pause_net.parameters(), lr=0.001)
+    optimizer = optim.Adam(pause_net.parameters(), lr=0.0005)
     loss_func = nn.BCELoss()
 
     # get data
@@ -60,25 +60,35 @@ if __name__ == '__main__':
         for song, labels in zip(reg_wavs, iso_labels):
 
             # sample window of 2048 and hop size of 512 samples
-            mfccs = librosa.feature.mfcc(y=song, n_mfcc=num_mfccs, center=False) #(num_mfccs, 5161)
+            mfccs = librosa.feature.mfcc(y=song, n_mfcc=num_mfccs) #(num_mfccs, 5168)
 
-            audio_length = song_dur / sr # in seconds
+            #breaks down mfccs into their time intervals
+            audio_length = len(song) / sr # in seconds
             step = hop_length / sr # in seconds
             intervals_s = np.arange(0, audio_length, step)
-            print(intervals_s)
 
-            for sec, label in zip(range(0, mfccs.shape[1], mfcc_sec), labels):
+            # get each second and sample
+            for label in labels:
+                #allows me to easily find the cutoff
+                intervals_s -= 1
+                #isolate a single second
+                sec_interval = np.where(intervals_s < 0)[0]
                 # indexes a single second of song from MFCCs
-                song_s = mfccs[:, sec:sec+mfcc_sec]
+                song_sec = np.take(mfccs, sec_interval, axis=1)
+
                 #average the values over a second
-                inp = np.mean(mfccs, axis=1)
+                inp = torch.from_numpy(np.mean(song_sec, axis=1))
+
+                label_tensor = torch.tensor([label])
 
                 optimizer.zero_grad()
                 pred = pause_net.forward(inp)
 
                 # nonpause proper label is 0, pause proper label is 1
-                loss = loss_func(pred, label)
+                loss = loss_func(pred, label_tensor)
                 total_loss += loss
 
                 loss.backward()
                 optimizer.step()
+
+        print("Epoch %i Total Loss: %.3f" % (epoch, total_loss))
